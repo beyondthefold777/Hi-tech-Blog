19e3999befe53e7903e983d56f254504ee15c10a
beyondthefold777 <beyondthefold1989@gmail.com>
2024-07-12 13:59:45 -0400

    added database url to env file for production deployment

--- models/index.js
+++ models/index.js
@@ -1,27 +1,5 @@
-require('dotenv').config();
+const sequelize = require('../config/connection');
 const { Sequelize } = require('sequelize');
-
-const env = process.env.NODE_ENV || 'development';
-
-let sequelize;
-
-if (env === 'production') {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  });
-} else {
-  sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: 'localhost', 
-    dialect: 'postgres'
-  });
-}
 
 const db = {};
 
